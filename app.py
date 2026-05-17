36b459d0960d1bf417efdce3560fb491c2e1bf6b
Richard Q Lin <richardqlin@gmail.com>
2026-05-17 10:31:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -192,5 +192,5 @@ def page_not_found(e):
 
 
 if __name__=="__main__":
-    app.run(host= "https://ancient-brook-99886.herokuapp.com/",debug = True)
+    app.run(host= "0.0.0.0",debug = True)
     #app.run(debug=True)
